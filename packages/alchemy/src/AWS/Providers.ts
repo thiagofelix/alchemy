b0bd5e7481776f7a3fa510fdf6d67f98b079889c
Thiago Felix <me@thiagofelix.com>
2026-08-28 09:37:33 -0300

    add AWS.Cognito.ManagedLoginBranding resource and provider

--- packages/alchemy/src/AWS/Providers.ts
+++ packages/alchemy/src/AWS/Providers.ts
@@ -419,6 +419,7 @@ export const providers = () =>
         Cognito.IdentityPool,
         Cognito.IdentityPoolRoleAttachment,
         Cognito.IdentityProvider,
+        Cognito.ManagedLoginBranding,
         Cognito.ResourceServer,
         Cognito.User,
         Cognito.UserPool,
@@ -1198,6 +1199,9 @@ export const providers = () =>
           ),
           flociDual(Cognito.IdentityProvider, () =>
             Cognito.IdentityProviderProvider(),
+          ),
+          flociDual(Cognito.ManagedLoginBranding, () =>
+            Cognito.ManagedLoginBrandingProvider(),
           ),
           flociDual(Cognito.ResourceServer, () =>
             Cognito.ResourceServerProvider(),
